83827d24d9efd576d9936ad172942a85fdbbb532
M4ry2 <115713170+M4ry2@users.noreply.github.com>
2023-05-05 12:11:37 +0100

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Community from '../components/Community';
+import Footer from '../components/Footer';
+import Hero from '../components/Hero';
+import MobileNav from '../components/MobileNav';
+import Navbar from '../components/Navbar';
+import Partners from '../components/Partners';
+import People from '../components/People';
+import SectionOne from '../components/SectionOne';
+import SectionThree from '../components/SectionThree';
+import SectionTwo from '../components/SectionTwo';
+import Statistics from '../components/Statistics';
+import Testimonies from '../components/Testimonies';
+import { Slide } from "react-awesome-reveal";
+
+const Home = ({open,setOpen}) => {
+  
+  return (
+    <>
+       <MobileNav
+       open={open}
+       setOpen={setOpen}
+       />
+       <Navbar
+        open={open}
+        setOpen={setOpen}
+       />
+       <Slide>
+  
+       <Hero/>
+       <SectionOne/>
+       <Partners/>
+       <SectionTwo/>
+       <Community/>
+       <SectionThree/>
+       <Statistics/>
+       <Testimonies/>
+       <People/>
+       <Footer/>
+       </Slide>
+       
+       
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/Login.jsx
@@ -0,0 +1,138 @@
+import React from 'react'
+import { FaArrowLeft, FaTimes } from 'react-icons/fa'
+import styles from './SignUp.module.css'
+import Slider from '../components/AuthSlider'
+import { useNavigate } from 'react-router-dom';
+
+
+
+const Login = () => {
+    const navigate= useNavigate();
+
+    const prevPage=()=>{
+        navigate(-1)
+    }
+  return (
+    <div className={styles.container}>
+        <div className={styles.inner}>
+            <div className={styles.left}>
+                {/* <Slider/> */}
+            </div>
+
+            <div className={styles.right}>
+                <FaTimes
+                onClick={prevPage}
+                className={styles.faTimes}
+                />
+
+                <div className={styles.header}>
+                    <FaArrowLeft/>
+
+                    <h2>Hello Moodies!</h2>
+                    <div></div>
+                </div>
+
+
+                <div className={styles.formSection}>
+                    <form>
+                        <div
+                        
+                        >
+                        <label>
+                           <p>First name</p> 
+                            <input
+                            type='text'                        
+                            />
+                        </label>
+                        {/* <label>
+                            <p>Last name</p>
+                            <input
+                            type='text'                        
+                            />
+                        </label> */}
+                        </div>
+                        
+
+                        <div>
+                        <label>
+                            <p>Email</p>
+                            
+                            <input
+                            type='text'                        
+                            />
+                        </label>
+
+                        {/* <label>
+
+                            <p>Phone Number</p>
+                            
+                            <input
+                            type='text'                        
+                            />
+                        </label> */}
+                        
+                        </div>
+
+                        <div>
+                        <label>
+                            
+                            <p>Password</p>
+                            <input
+                            type='text'                        
+                            />
+                        </label>
+
+                        {/* <label>
+                            <p>Confirm Password</p>
+                            
+                            <input
+                            type='text'                        
+                            />
+                        </label> */}
+                        </div>
+                    </form>
+                </div>
+
+                <div
+                className={styles.agree}
+                >
+                    {/* <input
+                    type='checkbox'
+                    />
+                    <span>By clicking here, I state that I have read and understood the terms and conditions</span> */}
+                    
+                    <button
+                    style={{width:'95%'}}
+                    >
+                        Sign Up
+                    </button>
+
+                    {/* <p>Already have an account? <span style={{color:'#5F51C2'}}>Log In</span></p> */}
+
+                    <div className={styles.line}>
+                        <div></div>
+                        <p>Or sign up with</p> 
+                        <div></div>
+                    </div>
+
+                    <div>
+                        <img
+                        src='/googlelogo.png'
+                        alt='google'
+                        />
+
+                        <img src="
+                        /fblogo.png
+                        " alt="
+                        fblogo" />
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
